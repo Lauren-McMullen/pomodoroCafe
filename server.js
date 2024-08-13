973d13f031d8d3edca1d5bfc558feb2fd9a1e7ea
lmcmulle <lmcmulle@students.cs.ubc.ca>
2024-08-13 09:22:55 -0700

    Set up partials for ejs views

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const app = express();
 
+// Configure express app 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-    res.render('./index');
-});
+
 
 // Mount Routers
 const loginRouter = require('./routes/login');
 const signupRouter = require('./routes/signup');
+const indexRouter = require('./routes/index');
 app.use('/login', loginRouter);
 app.use('/signup', signupRouter);
+app.use('/', indexRouter);
 
 app.use('/public', express.static('public'));
 
-// Setup Port
-const port = 50000;
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-});
+// Set up Port
+app.listen(process.env.PORT || 50000);
+
 
