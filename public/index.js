54e74a192fda5396f3037f19f29c92a59a53696d
lmcmulle <lmcmulle@students.cs.ubc.ca>
2024-08-22 16:56:09 -0700

    Adjust css to make pomo timer element more responsive

--- public/index.js
+++ public/index.js
@@ -9,15 +9,15 @@ let timer = {
     round: 1
 };
 
-breakText.textContent = breakSlider.value;
-workText.textContent = workSlider.value;
+breakText.textContent = `${breakSlider.value} minutes`;
+workText.textContent = `${workSlider.value} minutes`;
 
 breakSlider.oninput = function() {
-    breakText.textContent = this.value;
+    breakText.textContent = `${this.value} minutes`;
 }
 
 workSlider.oninput = function() {
-    workText.textContent = this.value;
+    workText.textContent = `${this.value} minutes`;
 }
 
 function startTimer() {
@@ -31,15 +31,15 @@ function startTimer() {
 
 window.onload = function () {
     document.getElementById('short-timer').addEventListener('click', (e) => {
-        workText.textContent = '25';
+        workText.textContent = '25 minutes';
         workSlider.value = 25;
-        breakText.textContent = '5';
+        breakText.textContent = '5 minutes';
         breakSlider.value = 5;
     });
     document.getElementById('long-timer').addEventListener('click', (e) => {
-        workText.textContent = '50';
+        workText.textContent = '50 minutes';
         workSlider.value = 50;
-        breakText.textContent = '10';
+        breakText.textContent = '10 minutes';
         breakSlider.value = 10;
     });
     document.getElementById('start-timer').addEventListener('click', startTimer);
